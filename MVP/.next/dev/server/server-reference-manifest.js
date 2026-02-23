126528a9257a78bc4c5d03ca8a9159c9af87e130
GonzaloCAZA <163890053+GonzaloCAZA@users.noreply.github.com>
2026-02-23 14:11:54 +0100

    gitignore adds

--- MVP/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HAaA/zZ6Or8wiA9oToM2+ZOEDbWV+2GalBM/34qTxPI=\"\n}"
--- MVP/.next/dev/static/chunks/app_dashboard_page_tsx_893e5ca7._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_e62a4322._.js",
-  "static/chunks/node_modules_0e6b4b03._.js"
-],
-    source: "dynamic"
-});
