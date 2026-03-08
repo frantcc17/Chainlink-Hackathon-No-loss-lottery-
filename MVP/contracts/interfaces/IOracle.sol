99c8f67b7bab8cea6a90c9dd0302dc5c2c96388f
frantcc17 <frantcc@hotmail.com>
2026-03-08 17:28:03 +0100

    Delete MVP/contracts directory

--- MVP/contracts/interfaces/IOracle.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-interface IOracle {
-    // Devuelve el precio de 1 USDY en términos de USDC (escalado a 18 decimales)
-    function getPrice() external view returns (uint256); 
-}
--- MVP/contracts/interfaces/IPrizePool.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT 
-
-pragma solidity ^0.8.28;
-interface IPrizePool {
-    function addYield(uint256 amount) external;
-}
